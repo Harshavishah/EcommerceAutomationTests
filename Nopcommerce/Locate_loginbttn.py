4de55a851a2ad4dcbbc69fb39cc6f8cb0e134f05
Harshavi Shah <harshavi-shah@outlook.com>
2023-11-13 12:16:57 -0500

    Clean Up un-used imports and Update Read.Md.

--- Nopcommerce/Locate_loginbttn.py
+++ Nopcommerce/Locate_loginbttn.py
@@ -1,11 +1,7 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-
-from selenium.webdriver.edge.service import service
 from selenium.webdriver.common.by import By
 
 browser =webdriver.Edge()
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # EcommerceAutomationTests
 Automation tests are a great way to ensure that your e-commerce website is working as expected and delivering a good user experience.
-Login Automation Test
+
+- Login Automation Test
